f008549cfce50cca04fd8c2e483bc46b822f1b4e
Mfon-obong Umoh <mfonobongumoh75@gmail.com>
2025-07-25 02:05:00 +0100

    update made

--- frontend/hashedpass.js
+++
@@ -1,5 +0,0 @@
-import bcrypt from 'bcryptjs';
-
-const password = 'admin123'; // change as needed, hash in production
-const hashedPassword = bcrypt.hashSync(password, 10);
-console.log('Hashed Password:', hashedPassword);
